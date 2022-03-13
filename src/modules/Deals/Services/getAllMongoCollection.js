e4f3dcacb14cd7ae24a44407851e355affedc4a1
gmorcatti <gabriel.morcatti@yahoo.com.br>
2022-03-13 12:05:52 -0300

    Create NodeCache in request

--- src/modules/Deals/Services/getAllMongoCollection.js
+++ src/modules/Deals/Services/getAllMongoCollection.js
@@ -1,11 +1,36 @@
+import NodeCache from 'node-cache';
+const nodeCache = new NodeCache();
+
 class GetAllMongoCollection {
     constructor({ mongoRepository }) {
         this.mongoRepository = mongoRepository;
     }
 
     async handle() {
+        // if there's a cached collection the API doesn't need to request to Mongo each time a request is made.
+        const cacheAllCollection = nodeCache.get( "allDailyAggregate" );
+
+        if(cacheAllCollection) {
+            console.log('Get the cached collection');
+            return cacheAllCollection;
+        }
+
         const allCollection = await this.mongoRepository.getAllDailyAggregate();
+
+        // Create a cache for get the daily aggregate.
+        const secondsUntilTheEndOfTheDay = this.getSecondsUntilTheEndOfTheDay();
+        nodeCache.set("allDailyAggregate", allCollection, secondsUntilTheEndOfTheDay);
+
         return allCollection;
+    }
+
+    getSecondsUntilTheEndOfTheDay() {
+        const date = new Date();
+        const hour = date.getHours();
+        const minutes = date.getMinutes();
+        const seconds = date.getSeconds();
+        const secondsUntilEndOfDay = (24 * 60 * 60) - (hour * 60 * 60) - (minutes * 60) - seconds;
+        return secondsUntilEndOfDay;
     }
 }
 
